26cf2c8072f5db3b279ff5bee18e28552bb1ad6e
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-09-26 14:46:09 +0500

    modify page_analyzer/app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -6,11 +6,8 @@ from flask import (
     redirect,
     url_for,
     flash,
-    get_flashed_messages
     )
-import psycopg2
 import validators
-import datetime
 import requests
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
@@ -26,39 +23,37 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 db.create_db_tables()
 
+
 @app.route('/')
 def index():
     url = request.args.get('url', '')
     return render_template('index.html', url=url)
 
+
 @app.route('/urls', methods=['GET', 'POST'])
 def show_urls():
     if request.method == 'POST':
         url = request.form['url']
-        
-        # Проверка валидности URL
         if not validators.url(url) or len(url) > 255:
             flash("Некорректный URL", 'danger')
             return redirect(url_for('index', url=url))
-
         existing_url = db.url_exists(url)
-        
         if existing_url:
             flash("Страница уже существует", 'warning')
             return redirect(url_for('show_url', id=existing_url[0]))
-
         url_id = db.add_url(url)
         flash("Страница успешно добавлена", 'success')
         return redirect(url_for('show_url', id=url_id))
-
     urls = db.get_all_urls()
     return render_template('urls.html', urls=urls)
+
 
 @app.route('/urls/<int:id>', methods=['GET'])
 def show_url(id):
     url = db.get_url_by_id(id)
     checks = db.get_url_checks(id)
     return render_template('url.html', url=url, checks=checks)
+
 
 @app.route('/urls/<int:id>/checks', methods=['POST'])
 def check_url(id):
@@ -70,7 +65,6 @@ def check_url(id):
     title = soup.title.string if soup.title else ''
     description = soup.find('meta', attrs={'name': 'description'})
     description = description['content'] if description else ''
-    
     db.add_url_check(id, status_code, h1, title, description)
     flash("Проверка успешно завершена", 'success')
     return redirect(f'/urls/{id}')
