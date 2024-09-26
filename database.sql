c218b716791e75aa6a8970e3babce259f223220d
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-09-26 14:58:12 +0500

    modify database.sql

--- database.sql
+++ database.sql
@@ -1,7 +1,3 @@
-drop table if exists urls;
-drop table if exists url_checks;
-
-
 create table urls (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255) UNIQUE NOT NULL,
