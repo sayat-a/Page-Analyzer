804989c87bd4b63cf29114021d478325e5fdc148
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-10-01 20:44:52 +0500

    modify page_analyzer/app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -7,12 +7,11 @@ from flask import (
     url_for,
     flash,
 )
-import validators
 import requests
-from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-from urllib.parse import urlparse
-from . import db
+from page_analyzer.validator import validate_url, normalize_url
+from page_analyzer import db
+from page_analyzer.parser import parse_url
 
 
 load_dotenv()
@@ -35,11 +34,10 @@ def index():
 def show_urls():
     if request.method == 'POST':
         url = request.form['url']
-        if not validators.url(url) or len(url) > 255:
+        if not validate_url(url) or len(url) > 255:
             flash("Некорректный URL", 'danger')
             return render_template('index.html', url=url), 422
-        parsed_url = urlparse(url)
-        normalized_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
+        normalized_url = normalize_url(url)
         existing_url = db.url_exists(normalized_url)
         if existing_url:
             flash("Страница уже существует", 'warning')
@@ -62,17 +60,16 @@ def show_url(id):
 def check_url(id):
     url = db.get_url_by_id(id)['name']
     try:
-        response = requests.get(url)
-        response.raise_for_status()
+        result = parse_url(url)
+        db.insert_url_check(
+            id,
+            result['status_code'],
+            result['h1'],
+            result['title'],
+            result['description']
+            )
+        flash("Страница успешно проверена", 'success')
+        return redirect(f'/urls/{id}')
     except requests.exceptions.RequestException:
         flash("Произошла ошибка при проверке", "danger")
         return redirect(url_for("show_url", id=id))
-    status_code = response.status_code
-    soup = BeautifulSoup(response.text, 'html.parser')
-    h1 = soup.h1.string if soup.h1 else ''
-    title = soup.title.string if soup.title else ''
-    description = soup.find('meta', attrs={'name': 'description'})
-    description = description['content'] if description else ''
-    db.insert_url_check(id, status_code, h1, title, description)
-    flash("Страница успешно проверена", 'success')
-    return redirect(f'/urls/{id}')
