e2037c44226e2166e41279ecc0c1ebcef7b2d6b1
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-10-01 21:36:55 +0500

    modify page_analyzer/app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -8,10 +8,11 @@ from flask import (
     flash,
 )
 import requests
+import psycopg2
 from dotenv import load_dotenv
 from page_analyzer.validator import validate_url, normalize_url
-from page_analyzer import db
 from page_analyzer.parser import parse_url
+from page_analyzer.db import UrlRepository
 
 
 load_dotenv()
@@ -19,9 +20,11 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 
-db.create_db_tables()
+conn = psycopg2.connect(DATABASE_URL)
+repo = UrlRepository(conn)
 
 
 @app.route('/')
@@ -38,35 +41,35 @@ def show_urls():
             flash("Некорректный URL", 'danger')
             return render_template('index.html', url=url), 422
         normalized_url = normalize_url(url)
-        existing_url = db.url_exists(normalized_url)
+        existing_url = repo.url_exists(normalized_url)
         if existing_url:
             flash("Страница уже существует", 'warning')
             return redirect(url_for('show_url', id=existing_url[0]))
-        url_id = db.insert_url(normalized_url)
+        url_id = repo.insert_url(normalized_url)
         flash("Страница успешно добавлена", 'success')
         return redirect(url_for('show_url', id=url_id))
-    urls = db.get_all_urls()
+    urls = repo.get_all_urls()
     return render_template('urls.html', urls=urls)
 
 
 @app.route('/urls/<int:id>', methods=['GET'])
 def show_url(id):
-    url = db.get_url_by_id(id)
-    checks = db.get_url_checks(id)
+    url = repo.get_url_by_id(id)
+    checks = repo.get_url_checks(id)
     return render_template('url.html', url=url, checks=checks)
 
 
 @app.route('/urls/<int:id>/checks', methods=['POST'])
 def check_url(id):
-    url = db.get_url_by_id(id)['name']
+    url = repo.get_url_by_id(id)['name']
     try:
-        result = parse_url(url)
-        db.insert_url_check(
+        response = parse_url(url)
+        repo.insert_url_check(
             id,
-            result['status_code'],
-            result['h1'],
-            result['title'],
-            result['description']
+            response['status_code'],
+            response['h1'],
+            response['title'],
+            response['description']
         )
         flash("Страница успешно проверена", 'success')
         return redirect(f'/urls/{id}')
