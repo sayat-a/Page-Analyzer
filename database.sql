bed207e43cc3b7fcbf124bcd3d9f9212bb8beb0a
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-10-08 01:04:30 +0500

    modify database.sql

--- database.sql
+++ database.sql
@@ -1,10 +1,10 @@
-create table urls if not exists(
+create table if not exists urls (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255) UNIQUE NOT NULL,
     created_at DATE
 );
 
-create table url_checks if not exists(
+create table if not exists url_checks (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     url_id BIGINT REFERENCES urls (id),
     status_code BIGINT,
