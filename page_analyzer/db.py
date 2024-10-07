624b8331beb7f124dfc7af1f9e70e8b23f50dc8a
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-10-08 01:01:59 +0500

    modify page_analyzer/db.py

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,15 +1,14 @@
 import datetime
-import os
 import psycopg2
 from psycopg2.extras import DictCursor
 
 
-class UrlRepository:
+class DatabaseConnectionManager:
     def __init__(self, database_url):
-        self.conn = psycopg2.connect(database_url)
+        self.database_url = database_url
 
     def __enter__(self):
-        self.conn = psycopg2.connect(os.getenv('DATABASE_URL'))
+        self.conn = psycopg2.connect(self.database_url)
         self.cur = self.conn.cursor(cursor_factory=DictCursor)
         return self
 
@@ -21,9 +20,14 @@ class UrlRepository:
         self.cur.close()
         self.conn.close()
 
+
+class UrlRepository:
+    def __init__(self, database_url):
+        self.database_url = database_url
+
     def get_all_urls(self):
-        with self as repo:
-            repo.cur.execute("""
+        with DatabaseConnectionManager(self.database_url) as conn:
+            conn.cur.execute("""
                 SELECT urls.id, urls.name, last_check.created_at AS last_check,
                 last_check.status_code
                 FROM urls
@@ -34,36 +38,36 @@ class UrlRepository:
                 ) AS last_check ON urls.id = last_check.url_id
                 ORDER BY urls.id DESC
             """)
-            urls = repo.cur.fetchall()
+            urls = conn.cur.fetchall()
         return urls
 
     def insert_url(self, url):
-        with self as repo:
-            repo.cur.execute("""
+        with DatabaseConnectionManager(self.database_url) as conn:
+            conn.cur.execute("""
                 INSERT INTO urls (name, created_at) VALUES (%s, %s)
                 RETURNING id
             """, (url, datetime.datetime.now()))
-            url_id = repo.cur.fetchone()[0]
+            url_id = conn.cur.fetchone()[0]
         return url_id
 
     def get_url_by_id(self, url_id):
-        with self as repo:
-            repo.cur.execute("SELECT * FROM urls WHERE id = %s", (url_id,))
-            url = repo.cur.fetchone()
+        with DatabaseConnectionManager(self.database_url) as conn:
+            conn.cur.execute("SELECT * FROM urls WHERE id = %s", (url_id,))
+            url = conn.cur.fetchone()
         return url
 
     def get_url_checks(self, url_id):
-        with self as repo:
-            repo.cur.execute("""
+        with DatabaseConnectionManager(self.database_url) as conn:
+            conn.cur.execute("""
                 SELECT * FROM url_checks WHERE url_id = %s
                 ORDER BY created_at DESC
             """, (url_id,))
-            checks = repo.cur.fetchall()
+            checks = conn.cur.fetchall()
         return checks
 
     def insert_url_check(self, check_params):
-        with self as repo:
-            repo.cur.execute("""
+        with DatabaseConnectionManager(self.database_url) as conn:
+            conn.cur.execute("""
                 INSERT INTO url_checks (url_id, status_code, h1,
                                         title, description, created_at)
                 VALUES (%s, %s, %s, %s, %s, %s)
@@ -76,7 +80,7 @@ class UrlRepository:
                 datetime.datetime.now()))
 
     def url_exists(self, url):
-        with self as repo:
-            repo.cur.execute("SELECT id FROM urls WHERE name = %s", (url,))
-            existing_url = repo.cur.fetchone()
+        with DatabaseConnectionManager(self.database_url) as conn:
+            conn.cur.execute("SELECT id FROM urls WHERE name = %s", (url,))
+            existing_url = conn.cur.fetchone()
         return existing_url
